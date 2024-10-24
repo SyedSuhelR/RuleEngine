0100fa6336c0ceeb9230574f43a7a538104aa4aa
syed suhel R <syedsuhel1119@gmail.com>
2024-10-24 19:32:03 +0530

    backend file

---
+++ demo/src/test/java/com/RuleEngine/demo/RuleEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RuleEngine.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
